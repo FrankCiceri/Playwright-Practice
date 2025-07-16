ea6ade47e3e59e740923c1c8185304c400bb6519
FrankCiceri <frankanthonyc00@gmail.com>
2025-07-15 19:05:42 -0500

    Added locators for Career tab

--- Playwright-Practice.Business/PageObjects/HeaderPage/HeaderPage.locators.cs
+++ Playwright-Practice.Business/PageObjects/HeaderPage/HeaderPage.locators.cs
@@ -11,5 +11,20 @@ namespace Playwright_Practice.Business.PageObjects.HeaderPage
     {
         private ILocator CareersButton => _page.Locator("a[class*= 'top-navigation__item-link'][href*='/careers']");
 
+        private ILocator CareersNavigationLink => _page.Locator("li").Filter(new LocatorFilterOptions
+        {
+            Has = CareersButton,
+        });
+
+        private ILocator CareersMenu => CareersNavigationLink.Locator(".top-navigation__flyout");
+
+        private ILocator MenuItem(string itemName) => CareersMenu.Locator("li").Filter(new LocatorFilterOptions()
+        {
+            HasText = itemName
+        });
+
+        private ILocator JoinOurTeamButton => MenuItem("Join our Team");
+
+
     }
 }
