5817fc148a2c7f978ea2caf9846b856c012df1cd
FrankCiceri <frankanthonyc00@gmail.com>
2025-07-19 20:38:36 -0500

    Home page and NavigateToCareersPageFromHeader() Created

---
+++ Playwright-Practice.Business/PageObjects/HomePage/HomePage.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.Playwright;
+using Playwright_Practice.Business.PageObjects.BasePages;
+
+namespace Playwright_Practice.Business.PageObjects.HomePage
+{
+    public partial class HomePage : BasePage
+    {        
+        public HomePage(IPage page) : base(page)
+        {         
+        }
+
+        public async Task NavigateToCareersPageFromHeader()
+        {
+            await Header.NavigateToCareersPage();
+        }
+
+    }
+}
