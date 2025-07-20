c3d1943dc895cb3d82dc2fb9d7bc5ec4ebb6bf52
FrankCiceri <frankanthonyc00@gmail.com>
2025-07-19 20:40:20 -0500

    Refactored test

--- Playwright-Practice/Test.cs
+++ Playwright-Practice/Test.cs
@@ -2,7 +2,7 @@ using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using Playwright_Practice.Business;
 using Playwright_Practice.Business.PageObjects.CareersPage;
-using Playwright_Practice.Business.PageObjects.HeaderPage;
+using Playwright_Practice.Business.PageObjects.HomePage;
 using Playwright_Practice.Core.Core;
 using Playwright_Practice.Core.Interfaces;
 using Playwright_Practice.UIHelper;
@@ -33,24 +33,21 @@ namespace Playwright_Practice
             });
             _page = await _browserContext.NewPageAsync();
             await _page.GotoAsync("https://www.epam.com");
+            await UiHelpers.AcceptCookiesIfVisibleAsync(_page);
         }
 
         [TestCase("Python", "Buenos Aires")]
-        public async Task Test1(string keyword, string location)
-        {
-            await UiHelpers.AcceptCookiesIfVisibleAsync(_page);
+        public async Task Test_SearchJobsViaHeaderAndCareersPage(string keyword, string location)
+        {            
 
-            var headerPage = new HeaderPage(_page);            
-            await headerPage.HoverCareersLink();
-            await headerPage.ClickJoinOurTeamButton();
+            var homePage = new HomePage(_page);
+            await homePage.NavigateToCareersPageFromHeader();
+            
 
-            //var careerPage = new CareersPage(_page);
-            //await careerPage.EnterSearchKeyword(keyword);
-            //await careerPage.SelectLocationFromDropdown(location);
-            //await careerPage.ClickRemoteCheckContainer();
-            //await careerPage.ClickJobSearchButton();
-            
-            
+            var careerPage = new CareersPage(_page);
+            await careerPage.SearchJobByFilterRemote(keyword, location);
+
+
         }
 
 
