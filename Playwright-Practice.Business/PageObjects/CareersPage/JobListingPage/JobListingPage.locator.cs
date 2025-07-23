731a55972abd3e57f255645061163d33e947e14c
FrankCiceri <frankanthonyc00@gmail.com>
2025-07-22 22:14:39 -0500

    Added locators for elements in job list

--- Playwright-Practice.Business/PageObjects/CareersPage/JobListingPage/JobListingPage.locator.cs
+++ Playwright-Practice.Business/PageObjects/CareersPage/JobListingPage/JobListingPage.locator.cs
@@ -5,9 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Playwright_Practice.Business.PageObjects.CareersPage
+namespace Playwright_Practice.Business.PageObjects.CareersPage.JobListingPage
 {
     public partial class JobListingPage
     {
+        private ILocator SortByDateBtn => Page.GetByTitle("Date");
+        private ILocator JobList => Page.Locator(".search-result__list > li");
+
+        private ILocator ApplyBtn(ILocator locator) => locator.GetByText("View and Apply");
     }
 }
